fbd2b326fb6eeac75858b08b9e9c1acfec33da9e
Horacio Aldo Tore <96441292+HoracioAldoTore@users.noreply.github.com>
2025-10-03 15:46:56 -0300

    Primera version

---
+++ AppIndexedDB/ExampleDb.cs
@@ -0,0 +1,42 @@
+﻿using Blazor.IndexedDB;
+using Microsoft.JSInterop;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AppIndexedDB.Data
+{
+    public class ExampleDb : IndexedDb
+    {
+        public ExampleDb(IJSRuntime jSRuntime, string name, int version) : base(jSRuntime, name, version) 
+        {
+            
+        }
+
+        // Define your object stores here
+        public IndexedSet<Employee> Employees { get; set; }
+        public IndexedSet<Product> Products { get; set; }
+    }
+
+    public class Employee : IEntity
+    {
+        [Key]
+        public int? Id { get; set; }
+        
+        public string? FirstName { get; set; }
+        
+        public string? LastName { get; set; }
+    }
+
+    public class Product : IEntity
+    {
+        [Key]
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public interface IEntity
+    {
+        int? Id { get; set; }
+    }
+}
